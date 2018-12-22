76aa247d4b0ab9da5aff5c5c02892419dea1432f
Kris Sankaran <sankaran.kris@gmail.com>
2018-12-21 19:15:12 -0500

    generate sigmoid plots

---
+++ notebooks/logistic_boundary.R
@@ -0,0 +1,47 @@
+
+library("tidyverse")
+library("viridis")
+sigmoid <- function(pi) {
+  1 / (1 + exp(-pi))
+}
+
+generate_probs <- function(sigma=2) {
+  theta <- rnorm(3, 0, sigma)
+  u <- seq(-1, 1, 0.01)
+  x <- list()
+  k <- 1
+  for (i in seq_along(u)) {
+    for (j in seq_along(u)) {
+      x[[k]] <- c(u[i], u[j])
+      k <- k + 1
+    }
+  }
+
+  x <- do.call(rbind, x) %>%
+    as_data_frame()
+  x$intercept <- 1
+  x$prob <- sigmoid(t(theta %*% t(x)))
+  x
+}
+
+plot_probs <- function(x) {
+  ggplot(x) +
+    geom_tile(
+      aes(
+        x = V1,
+        y = V2,
+        fill = prob
+      )
+    ) +
+    geom_vline(xintercept = 0) +
+    geom_hline(yintercept = 0) +
+    scale_x_continuous("x[1]", expand = c(0, 0)) +
+    scale_y_continuous("x[2]", expand = c(0, 0)) +
+    scale_fill_viridis()
+}
+
+for (i in seq_len(10)) {
+  x <- generate_probs()
+  p <- plot_probs(x)
+  ggsave(sprintf("sigmoid_plot_%s.png", i))
+}
