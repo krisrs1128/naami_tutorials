64f5227303c54f92a082d1151567780e9a569bad
Kris Sankaran <sankaran.kris@gmail.com>
2018-12-25 12:37:56 -0500

    update some of the experiments

--- notebooks/autograd.py
+++ notebooks/autograd.py
@@ -1,3 +1,5 @@
+#!/usr/bin/env python
+# Exercises using automatic differentiation
 
 import torch
 import numpy as np
--- notebooks/logistic.py
+++ notebooks/logistic.py
@@ -1,12 +1,30 @@
 #!/usr/bin/env python
 
+"""
+An introduction to pytorch nn modules via logistic regression. A challenge at
+the end for fitting a nonlinear decision boundary.
+"""
 import torch
 import numpy as np
 import matplotlib.pyplot as plt
 
+###############################################################################
+# Generic functions
+###############################################################################
+def sigmoid(x):
+    return 1 / (1 + np.exp(-x))
 
 
 def mixture_of_gaussians(n, k=2, pi=None):
+    """
+    Simulate 2D Mixture of Gaussians
+
+    :param n: The number of points to simulate.
+    :param k: The number of mixture components.
+    :param pi: The marginal probabilities of different classes.
+    :return mean, data: A tuple giving (1) the means for the mixture components
+      and (2) the positions of the mixture of gaussians points
+    """
     if not pi:
         pi = np.ones(k) / k
 
@@ -19,11 +37,20 @@ def mixture_of_gaussians(n, k=2, pi=None):
 
     return means, data
 
-def sigmoid(x):
-    return 1 / (1 + np.exp(-x))
 
-# mixture of gaussians data
-def mog_classes(x, theta, k=2):
+def mix_gauss_classes(x, theta):
+    """
+    Label a Mixture of Gaussians
+
+    This simulates points x using a mixture of gaussians and then defines
+    binary $y$ according to a sigmoid on theta ^T x
+
+    :param x: An n x 2 numpy array giving the x coordinates of the points of
+      interest.
+    :param theta: The direction used to define the class probabilities y
+    :return pi, y: A tuple giving the class probabilties and assignments for
+      each point x, respectively
+    """
     n, _ = x.shape
     x_tilde = np.hstack((np.ones((n, 1)), x))
     pi = sigmoid(np.dot(x_tilde, theta))
@@ -34,28 +61,10 @@ def mog_classes(x, theta, k=2):
 
     return pi, y
 
-n = 500
-means, x = mixture_of_gaussians(n)
-plt.scatter(x[:, 0], x[:, 1])
 
-theta = means[1, :] - means[0, :]
-theta = np.hstack((0.2, theta))
-theta /= np.sqrt(sum(theta ** 2))
-
-pi, y = mog_classes(x, theta)
-plt.scatter(x[:, 0], x[:, 1], c=y)
-plt.scatter(x[:, 0], x[:, 1], c=pi)
-
-# fit a logistic regression on this
-
-# now do a nonlinear transformation
-z = np.vstack([x[:, 0] ** 2 - x[:, 1] * x[:, 0], x[:, 0] ** 2 + x[:, 1] ** 2 - 2 * x[:, 0] * x[:, 1]]).T
-plt.scatter(z[:, 0], z[:, 1], c=pi)
-
-# fit a logistic regression on this new z
-# see if you can add a hidden layer to the logistic model to get a better
-# decision boundary
-
+###############################################################################
+# Logistic regression torch module
+###############################################################################
 
 class LogisticRegression(torch.nn.Module):
     def __init__(self, D):
@@ -67,22 +76,30 @@ class LogisticRegression(torch.nn.Module):
         Wx = self.linear(x)
         return self.output(Wx)
 
+
+###############################################################################
+# Simulation experiment
+###############################################################################
+n = 500
+means, x = mixture_of_gaussians(n)
+plt.scatter(x[:, 0], x[:, 1])
+
+theta = means[1, :] - means[0, :]
+theta = np.hstack((0.2, theta))
+theta /= np.sqrt(sum(theta ** 2))
+
+pi, y = mix_gauss_classes(x, theta)
+plt.scatter(x[:, 0], x[:, 1], c=y)
+plt.scatter(x[:, 0], x[:, 1], c=pi)
+
+# fit a logistic regression on this
 model = LogisticRegression(2)
 x_tens = torch.as_tensor(x, dtype=torch.float32)
 y_tens = torch.as_tensor(y, dtype=torch.float32)
 y_tens = y_tens.reshape((n, 1))
-y_hat = model.forward(x_tens) # totally random predictions
-plt.scatter(x[:, 0], x[:, 1], c=y_hat.detach().numpy().flatten())
-
-torch.nn.BCELoss()(input=torch.Tensor([0.1]), target=torch.Tensor([1.]))
 
 loss_fn = torch.nn.BCELoss()
 optimizer = torch.optim.SGD(model.parameters(), lr=0.01)
-
-# look at a few values
-loss_fn(torch.tensor([0.2]), torch.tensor([1.]))
-loss_fn(torch.tensor([0.9]), torch.tensor([1.]))
-loss_fn(torch.tensor([0.99]), torch.tensor([1.]))
 
 # train the model
 for i in range(500):
@@ -91,14 +108,21 @@ for i in range(500):
 
     optimizer.zero_grad()
     loss.backward()
-    print(loss.item())
     optimizer.step()
-    if i % 5 == 0:
-        fig = plt.figure()
-        plt.scatter(x[:, 0], x[:, 1], c=y_hat.detach().numpy().flatten())
-        name = "image_{}".format(str(i).rjust(4, "0"))
-        fig.savefig(name)
+
+    # Uncomment this to save images of the decision boundary being adjusted
+    # if i % 5 == 0:
+    #     print(loss.item())
+    #     fig = plt.figure()
+    #     plt.scatter(x[:, 0], x[:, 1], c=y_hat.detach().numpy().flatten())
+    #     name = "image_{}".format(str(i).rjust(4, "0"))
+    #     fig.savefig(name)
 
 
-y_hat = model.forward(x_tens) # totally random predictions
-plt.scatter(x[:, 0], x[:, 1], c=y_hat.detach().numpy().flatten())
+# now do a nonlinear transformation
+z = np.vstack([x[:, 0] ** 2 - x[:, 1] * x[:, 0], x[:, 0] ** 2 + x[:, 1] ** 2 - 2 * x[:, 0] * x[:, 1]]).T
+plt.scatter(z[:, 0], z[:, 1], c=pi)
+
+# fit a logistic regression on this new z
+# see if you can add a hidden layer to the logistic model to get a better
+# decision boundary
